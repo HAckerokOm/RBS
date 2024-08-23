3b9f71b4e4143dc3e863073a1492661a859afc2b
HAckerokOm <zimskoj99@mail.ru>
2024-08-23 11:44:52 +0300

    fix

--- 22.go
+++ 22.go
@@ -14,63 +14,63 @@ import (
 )
 
 func main() {
-	inputFile, outputDir := parseFlags() 
+	inputFile, outputDir := parseFlags()// Парсим флаги командной строки для определения входного файла и директории вывода
 	startTime := time.Now() // время начала программы
 	fmt.Println("Программа выполняется...")
 
-	processInputFile(*inputFile, *outputDir)
+	processInputFile(*inputFile, *outputDir)// Обрабатываем входной файл
 
 	endTime := time.Now()
 	duration := endTime.Sub(startTime)
 	fmt.Printf("Время выполнения программы: %v\n", duration)
 }
 
-func isValidURL(url string) bool {
-	resp, err := http.Head(url)
-	if err != nil {
-		return false
-	}
-	defer resp.Body.Close()
-
-	return resp.StatusCode == http.StatusOK
-}
-
-func fetchHTML(url string) ([]byte, error) {
+func fetchHTML(url string) ([]byte, error) {// Функция для получения HTML-кода страницы по URL
 	resp, err := http.Get(url)
 	if err != nil {
-		return nil, fmt.Errorf("Ошибка получения %s: %v", url, err)
+		return nil, fmt.Errorf("Ошибка получения %s: %v", url, err)// Возвращение ошибки, если запрос к URL не удался
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, fmt.Errorf("ошибка чтения %s: %v", url, err)
+		return nil, fmt.Errorf("ошибка чтения %s: %v", url, err)// Возвращение ошибки, если чтение тела ответа не удалось
 	}
 
 	return body, nil
 }
 
-func parseFlags() (*string, *string) {
+func parseFlags() (*string, *string) {// Функция для разбора флагов командной строки
 	inputFile := flag.String("input", "./url.txt", "Путь к файлу с URL-адресами")
 	outputDir := flag.String("output", "./output", "Путь к директории для сохранения HTML-файлов")
-	flag.PrintDefaults() 
+	//flag.PrintDefaults()
 	flag.Parse()
 	setupOutputDir(*outputDir)
+	
+// Проверяем, были ли указаны пути к файлу и директории, отличные от значений по умолчанию
+	if *inputFile == " "{
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+		
+	if *outputDir  == " "{
+		flag.PrintDefaults()}
 	return inputFile, outputDir
 }
 
-func setupOutputDir(dirPath string) {
+
+func setupOutputDir(dirPath string) {// Функция для создания директории для вывода, если она еще не существует
 	err := os.MkdirAll(dirPath, os.ModePerm)
 	if err != nil {
-		fmt.Println("Ошибка создания директории", err)
+		fmt.Println("Ошибка создания директории", err)// Вывод сообщения об ошибке и выход из программы при возникновении
 		os.Exit(1)
 	}
 }
 
-func processInputFile(filePath string, outputDir string) {
+func processInputFile(filePath string, outputDir string) {// Функция для обработки входного файла с URL-адресами
 	file, err := os.Open(filePath)
 	if err != nil {
-		fmt.Println("ошибка открытия файла:", err)
+		fmt.Println("ошибка открытия файла:", err)// Вывод сообщения об ошибке и выход из программы при возникновении ошибки открытия файла
 		os.Exit(1)
 	}
 	defer file.Close()
@@ -81,7 +81,8 @@ func processInputFile(filePath string, outputDir string) {
 		url := scanner.Text()
 		url = strings.TrimSpace(url)
 		if url != "" {
-			if isValidURL(url) {
+			_, err := fetchHTML(url) // Попытка получить HTML
+			if err == nil { // Если нет ошибок, считаем URL валидным
 				fmt.Printf("валидная ссылка: %s\n", url)
 				wg.Add(1)
 				go processURL(url, &wg, i, outputDir)
@@ -92,33 +93,34 @@ func processInputFile(filePath string, outputDir string) {
 	}
 
 	if err := scanner.Err(); err != nil {
-		fmt.Println("Ошибка чтения файла:", err)
+		fmt.Println("Ошибка чтения файла:", err)// Вывод сообщения об ошибке сканирования файла и выход из программы
 		os.Exit(1)
 	}
 
 	wg.Wait()
 }
 
+// Функция для обработки каждого URL в отдельной горутине
 func processURL(url string, wg *sync.WaitGroup, i int, outputDir string) {
 	defer wg.Done()
 	fmt.Printf("Горутина для %s начата\n", url)
 	html, err := fetchHTML(url)
 	if err != nil {
-		fmt.Printf("Ошибка получения HTML для %s: %v\n", url, err)
+		fmt.Printf("Ошибка получения HTML для %s: %v\n", url, err)// Вывод сообщения об ошибке получения HTML и возврат из функции
 		return
 	}
 
 	filename := filepath.Join(outputDir, fmt.Sprintf("url_%d.html", i+1))
 	file, err := os.Create(filename)
 	if err != nil {
-		fmt.Printf("ошибка создания файла для %s: %v\n", url, err)
+		fmt.Printf("ошибка создания файла для %s: %v\n", url, err)// Вывод сообщения об ошибке создания файла и возврат из функции
 		return
 	}
 	defer file.Close()
 
 	_, err = file.Write(html)
 	if err != nil {
-		fmt.Printf("Ошибка записи HTML %s: %v\n", url, err)
+		fmt.Printf("Ошибка записи HTML %s: %v\n", url, err)// Вывод сообщения об ошибке записи HTML и возврат из функции
 	}
 	fmt.Printf("Горутина для %s завершена\n", url)
 }
